9617d4f6dee978b28f3a9064a24b4e6eec1b7d41
AyaMustafa <yoyamustafa22@gmail.com>
2022-01-25 10:41:13 +0200

    CreatingAllComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { RecipesComponent } from './component/recipes/recipes.component';
+import { RecipeListComponent } from './component/recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './component/recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './component/recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShopingListComponent } from './component/shoping-list/shoping-list.component';
+import { ShopingEditComponent } from './component/shoping-list/shoping-edit/shoping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShopingListComponent,
+    ShopingEditComponent
   ],
   imports: [
     BrowserModule,
